89f5680fb6e0cf23b8eb0e87ded28a13c802bc83
gensym <david@gensym.org>
2013-09-04 22:20:50 -0500

    Added available docks chart to station overview page

--- resources/public/js/station_updates.js
+++ resources/public/js/station_updates.js
@@ -1,4 +1,4 @@
-function update_station_graph(graph_selector, station_id ) {
+function update_available_bikes_graph(graph_selector, station_id ) {
 
   $.ajax("/available_bikes/" + station_id + ".edn").success(
     function(e) {
@@ -8,7 +8,31 @@ function update_station_graph(graph_selector, station_id ) {
                 update[":available-bikes"]];
       });
       
-      console.log(table);
+      var options = {
+        xaxis: {
+          mode: "time",
+        },
+        yaxis: {
+          ticks: function(axis) {
+            return _.range(axis.max);
+          }
+        }
+      };
+      $.plot(graph_selector, [table], options);
+    });
+}
+
+
+function update_available_docks_graph(graph_selector, station_id ) {
+
+  $.ajax("/available_docks/" + station_id + ".edn").success(
+    function(e) {
+      var updates = jsedn.toJS(jsedn.parse(e));
+      var table = _.map(updates, function(update) {
+        return [update[":execution-time"].getTime(), 
+                update[":available-docks"]];
+      });
+
       var options = {
         xaxis: {
           mode: "time",
