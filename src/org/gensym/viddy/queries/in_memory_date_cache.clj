fccb0b7cef655680b2c28611f0e55581006feec4
gensym <david@gensym.org>
2014-02-16 20:50:08 -0600

    Fixing a protocol-related compile error in the in-memory-date-cache sketch

--- src/org/gensym/viddy/queries/in_memory_date_cache.clj
+++ src/org/gensym/viddy/queries/in_memory_date_cache.clj
@@ -4,17 +4,20 @@
 (defn make-datacache [calculation-fns]
   (reify dc/DateCache
 
-    (maybe-find-for-date-range [this key start end]
+    (maybe-find-for-date-range [this key start end args]
       (if (contains? this [key start end])
         [true (get this [key start end])]
         [false nil]))
 
-    (calculate-for-date-range [this key start end]
+    (calculate-for-date-range [this key start end args]
       ((get calculation-fns key) start end))
 
 
-    (store-calculation [this key start end calculation]
+    (store-calculation [this key start end calculation args]
       (assoc this [key start end] calculation))
 
-    (concat-calculation [this a b]
-      (calculation-fns key) a b)))
+    (concat-calculation [this key a b]
+      (calculation-fns key) a b)
+
+    (empty-calculation [datasource key]
+      )))
