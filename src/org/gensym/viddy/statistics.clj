4e28688bb997f9f6e015b7f3380f05e6a24f1e62
gensym <david@gensym.org>
2014-03-01 21:55:39 -0600

    Removed redundant percentiles definition from statistics namespace

--- src/org/gensym/viddy/statistics.clj
+++ src/org/gensym/viddy/statistics.clj
@@ -1,25 +1,12 @@
-(ns org.gensym.viddy.statistics)
-
-(defn percentile [percentile coll]
-  "Percentile should be a number between 1 and zero"
-  (let [c (count coll)]
-    (if (= 0 c)
-      0
-      (nth coll (max 0 (dec
-                        (int (Math/ceil (* percentile c)))))))))
-
-(defn percentiles [keys coll]
-  (let [coll (vec coll)]
-    (reduce
-     (fn [m v]
-       (assoc m v (percentile v coll)))
-     {} keys)))
+(ns org.gensym.viddy.statistics
+  (:require [org.gensym.viddy.frequencies :as freq]))
 
 (defn analyse-percentages [group-by-fn value-fn percentile-keys coll]
   (->> coll
        (group-by group-by-fn)
-       (map (fn [[k v]] [k (percentiles percentile-keys
-                                       (sort (map value-fn v)))]))
+       (map (fn [[k v]] [k (freq/percentiles percentile-keys
+                                            (freq/create
+                                             (sort (map value-fn v))))]))
        (into {})))
 
 (defn rotate-keys [m]
--- test/org/gensym/viddy/statistics_test.clj
+++ test/org/gensym/viddy/statistics_test.clj
@@ -2,39 +2,6 @@
   (:require [org.gensym.viddy.statistics :as stats])
   (:use clojure.test))
 
-(deftest as-percentiles
-  (testing "Should split simple numbers into percentiles"
-    (let [expected {0.1 9
-                    0.3 29
-                    0.5 49
-                    0.8 79
-                    0.9 89}
-          inputs (range 100)
-          actual (stats/percentiles [0.1 0.3 0.5 0.8 0.9] inputs)]
-      (is (= expected actual))))
-  (testing "Should work with small collections"
-    (let [exected {0.1 1
-                   0.3 1
-                   0.5 1
-                   0.51 2
-                   0.8 2
-                   0.9 2
-                   1.0 2}
-          inputs [1 2]
-          actual (stats/percentiles [0.1 0.3 0.5 0.51 0.8 0.9 1.0] inputs)]
-      (is (= exected actual))))
-
-  (testing "Should work with a single-elemenet collection"
-    (let [expected {0.1 42
-                   0.5 42
-                   0.9 42}]
-      (is (= expected (stats/percentiles [0.1 0.5 0.9] [42])))))
-  (testing "Should work with an empty collection"
-    (let [expected {0.1 0
-                    0.5 0
-                    0.9 0}]
-      (is (= expected (stats/percentiles [0.1 0.5 0.9] []))))))
-
 (deftest series-percentiles
   (testing "Should break data down into percentiles"
     (let [input-data [
