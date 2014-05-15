d0aba884eaa0573cac99df2def82592c6af47afd
gensym <david@gensym.org>
2014-05-14 21:42:46 -0500

    parking mongo date cache

--- src/org/gensym/viddy/queries/mongo_date_cache.clj
+++ src/org/gensym/viddy/queries/mongo_date_cache.clj
@@ -1,11 +1,25 @@
 (ns org.gensym.viddy.queries.mongo-date-cache
-  (:require [monger.core :as mg])
+  (:require [monger.core :as mg]
+            [monger.multi.collection :as coll])
   (:import [com.mongodb MongoOptions ServerAddress]))
 
 (def host "localhost")
 
-(def port 7878)
+(def dbname "local")
+
+(def port 27017)
 
 (defn connect []
   (mg/connect {:host host :port port}))
 
+
+
+(defn insertion [db]
+  (coll/insert-and-return db "documents" {:name "John" :age 23 :hobbies ["fishing" "fission"]}))
+
+
+(defn doit [conn]
+  (insertion (mg/get-db conn dbname)))
+
+(defn findit [conn]
+  (coll/find (mg/get-db conn dbname) "documents" {:name "John"}))
